4ea4581c9e2ffaca637b15508263e31bb24eb60c
XPS\ktc <>
2026-04-25 16:26:15 +0900

    ４月２５日授業

---
+++ 2026_04_25_sakata/2026_04_25_sakata.cpp
@@ -0,0 +1,53 @@
+﻿// 2026_04_25_sakata.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() 
+{
+    // int answer = 5;
+
+    int t = (int)time(nullptr);
+    int answer = t % 10;
+    bool loop = true;
+	while (loop)
+	{
+        int input;
+
+        scanf_s("%d", &input);
+
+        int rank = 0;
+
+        int sub = answer - input;
+
+        if (answer == input)
+        {
+            rank = 2;
+            printf("一致!\n");
+        }
+        if (sub > -3 && answer != input)
+        {
+            if (sub < 3 && answer != input)
+            {
+                rank = 1;
+                printf("惜しい\n");
+            }
+        }
+        switch (rank)
+        {
+        case 2:
+            printf("ランクS\n");
+            loop = false;
+            break;
+        case 1:
+            printf("ランクA\n");
+            break;
+        default:
+            printf("ランクB\n");
+            break;
+        }
+	}
+    return 0;
+}
